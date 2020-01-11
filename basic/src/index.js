3ce777a73aaca6ed8d8b164d407b5e370e1fcde3
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-01-11 15:07:38 +0500

    Add data in json and xml, add xml-loader

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style/style.css';
 import Icon from './img/flower.png';
+import DataXML from './data/data.xml';
+import DataJSON from './data/data.json';
 
 function component() {
     const element = document.createElement('div');
@@ -10,6 +12,8 @@ function component() {
     const icon = new Image();
     icon.src = Icon;
     element.appendChild(icon);
+
+    console.log({DataXML, DataJSON});
 
     return element;
 }
