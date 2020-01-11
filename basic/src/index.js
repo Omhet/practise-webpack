27e7fb00611b559cbc3068d13eb5792652426489
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-01-11 14:48:19 +0500

    Add images and file-loader

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './flower.png';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const icon = new Image();
+    icon.src = Icon;
+    element.appendChild(icon);
+
     return element;
 }
 
