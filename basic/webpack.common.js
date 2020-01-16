709d4c091ff706bcec92a5713e6a8b0b1408ed48
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-01-16 22:46:42 +0300

    Split configuration into 2 parts: prod and dev

--- basic/webpack.common.js
+++ basic/webpack.common.js
@@ -7,29 +7,10 @@ const outputDir = path.resolve(__dirname, 'dist');
 module.exports = {
     entry: {
         main: './src/index.js',
-        // print: './src/js/print.js',
     },
     output: {
         filename: '[name].[contenthash].js',
-        // chunkFilename: '[name].bundle.js',
         path: outputDir
-    },
-    mode: 'development',
-    // devtool: 'inline-source-map',
-    devServer: {
-        contentBase: outputDir
-    },
-    optimization: {
-        runtimeChunk: 'single',
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all'
-                }
-            }
-        }
     },
     plugins: [
         new CleanWebpackPlugin(),
